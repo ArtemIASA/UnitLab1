e3526e99751d1e02b16dd7fb0e4bd8586dbd4a6b
ArtemIASA <arpogorelov@gmail.com>
2021-04-04 00:06:23 +0300

    Add project files.

---
+++ Class4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System.Linq;
+
+public class Task5
+{
+	struct human
+	{
+		public string name;
+		public string surname;
+		public human(string str)
+		{
+			string[] split_str = str.Split(":");
+			name = split_str[0];
+			surname = split_str[1];
+		}
+	}
+	public Task5()
+	{
+	}
+
+	public static string surname_order(string input)
+	{
+		List<human> list = new List<human>();
+		foreach(var str in input.Split(";"))
+		{
+			list.Add(new human(str));
+		}
+		var sorted_people = list.OrderBy(h => h.surname).ThenBy(h => h.name).ToList();
+		string result = "";
+		foreach (var human in sorted_people)
+		{
+			result = result + "(" + human.surname.ToUpper() + ", " + human.name.ToUpper() + ")";
+		}
+		return result;
+	}
+}
---
+++ Task1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Task1
+{
+	public Task1()
+	{
+
+	}
+
+	public static List<object> filterList(List<object> list)
+	{
+		var filtered = new List<object>();
+		foreach (var word in list)
+		{
+			if (word is int || word is uint)
+			{
+				filtered.Add(word);
+			}
+		}
+		return filtered;
+	}
+}
